8af318dc2cd5fc8253426035e033c37ed4bfdcaa
20222ireads0016-git <20222ireads0016@ifba.edu.br>
2025-11-29 00:10:08 -0300

    Adicionando o Bean Repository ao CursoRepository (Tava funcionando sem, mas achei por bom colocar)

--- pgr03paulohenrique-part3/src/main/java/br/com/ifba/curso/repository/CursoRepository.java
+++ pgr03paulohenrique-part3/src/main/java/br/com/ifba/curso/repository/CursoRepository.java
@@ -10,11 +10,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author paulo
  */
+@Repository
 public interface CursoRepository extends JpaRepository<Curso, Long>{
     
     Curso findByCodigoCurso(String codigoCurso);
